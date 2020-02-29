be54e53eb8f197040fe040cfe1c95095ba4ee18e
Will King <contact@wking.dev>
2020-02-29 11:48:12 -0600

    Tests WIP

--- test/online_ops_web/controllers/user_controller_test.exs
+++ test/online_ops_web/controllers/user_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule OnlineOpsWeb.UserControllerTest do
   use OnlineOpsWeb.ConnCase, async: true
 
+  import OnlineOps.Factory
+
   describe "GET /signup" do
     test "renders the registration form", %{conn: conn} do
       body =
@@ -9,6 +11,30 @@ defmodule OnlineOpsWeb.UserControllerTest do
         |> html_response(200)
 
       assert body =~ "data-submit=\"signup\""
+    end
+
+    test "redirects to dashboard if already logged in", %{conn: conn} do
+      user = insert(:user)
+
+      result =
+        conn
+        |> assign(:current_user, user)
+        |> get("/signup")
+        |> redirected_to(302)
+
+      assert result =~ "/app"
+    end
+  end
+
+  describe "POST /signup" do
+    test "creates a new user with valid input" do
+    end
+
+    test "creates magic link email with valid input" do
+    end
+
+    test "shows warning with redirect to /signin when using email that is already in use" do
+      
     end
   end
 end
