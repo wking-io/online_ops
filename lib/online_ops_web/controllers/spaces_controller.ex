07609260df95743c2e940b566c859f667c214ca4
Will King <contact@wking.dev>
2020-03-06 13:04:15 -0600

    Adding control structure for checking space and access

--- lib/online_ops_web/controllers/spaces_controller.ex
+++ lib/online_ops_web/controllers/spaces_controller.ex
@@ -4,8 +4,16 @@ defmodule OnlineOpsWeb.SpacesController do
   alias OnlineOps.Spaces
 
   def index(conn, _params) do
-    conn
-    |> render("index.html")
+    case Spaces.get_all(conn.assigns[:current_user]) do
+      {:ok, spaces} ->
+        conn
+        |> assigns(:spaces, spaces)
+        |> render("index.html")
+
+      {:error, _} ->
+        conn
+        |> render("index.html")
+    end
   end
 
   def edit(conn, %{"id" => _id}) do
@@ -19,15 +27,22 @@ defmodule OnlineOpsWeb.SpacesController do
   end
 
   def show(conn, %{"id" => id}) do
-    case Spaces.get_by_id(id) do
-      {:ok, space} ->
-        conn
-          |> assign(:space, space)
-          |> render("space.html")
-
-      {:error, _} ->
+    with
+      {:ok, space} <- Spaces.get_by_id(id),
+      {:ok, space_user} <- Spaces.get_user(conn.assign[:current_user])
+    do
+      conn
+        |> assign(:space, space)
+        |> assign(:current_space_user, space_user)
+        |> render("space.html")
+    else
+      {:error, :not_found} ->
         conn
           |> put_flash(:error, "That space does not exist.")
+          |> redirect(to: Routes.spaces_path(conn, :index))
+
+      {:error, :not_authorized} ->
+        conn
           |> redirect(to: Routes.spaces_path(conn, :index))
     end
   end
